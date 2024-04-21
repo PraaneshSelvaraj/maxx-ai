996196c77b6c96923604ed12433784c6e8cb3ca8
PraaneshSelvaraj <praaneshselvaraj2003@gmail.com>
2024-04-21 10:28:26 +0530

    Added Memory to Agent

--- core/agents/jsonChatAgent.py
+++ core/agents/jsonChatAgent.py
@@ -1,10 +1,12 @@
 from langchain.agents import create_json_chat_agent, AgentExecutor
+from langchain_core.messages import AIMessage, HumanMessage
 
 class JsonChatAgent:
     def __init__(self, llm, tools, prompt):
         self.llm = llm
         self.tools = tools
         self.prompt = prompt
+        self.memory = []
 
         self.agent_executor = AgentExecutor(
             agent=create_json_chat_agent(self.llm, self.tools, self.prompt),
@@ -14,5 +16,7 @@ class JsonChatAgent:
         )
 
     def invoke(self, query : str) -> str:
-        resp = self.agent_executor.invoke({"input" : query})
+        resp = self.agent_executor.invoke({"input" : query, 'chat_history' : self.memory})
+        self.memory.append(HumanMessage(content=query))
+        self.memory.append(AIMessage(content=resp['output']))
         return resp['output']
